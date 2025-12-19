fbc66e4e53efc1572d18d15138d5016b8f833194
wachirajam3s <jwachiramathenge@gmail.com>
2025-12-19 08:24:05 +0300

    DB config and models

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,58 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+from .config import db, migrate
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
     
-    @app.route('/')
+    # CORS Setup
+    allowed_origins = [
+        "https://volaplace-api.onrender.com",
+        "http://localhost:5174",
+    ]
+    CORS(app, origins=allowed_origins)
+
+    # from .env for local.
+    uri = os.environ.get('DATABASE_URL')
+
+    # handle both render and local postgres connections.
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # import models inside factory.
+    # this prevents circular imports and registers models with SQLAlchemy
+    with app.app_context():
+        from . import models
+
+    # simple routes.
+    @app.route('/', methods=['GET'])
     def index():
         return jsonify({"message": "VolaPlace API Running"})
     
-    @app.route('/api/health')
+    # endpoint health check.
+    @app.route('/api/health', methods=['GET'])
     def health():
         return jsonify({"status": "healthy"})
-    
+
+    # blueprint
+    from routes.search import api_bp
+    app.register_blueprint(api_bp)
+
+    # CLI seed command (flask seed)
+    @app.cli.command("seed")
+    def run_seed():
+        from seed import seed_database
+        seed_database()
+
     return app
